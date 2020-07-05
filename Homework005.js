65c20b8b36bc76388f9b561cc060efa1d558e120
shinoby85 <ponk@tut.by>
2020-07-05 19:28:52 +0300

    Выполнены задания 4-8. Подготовлено условие к 9.

--- Homework005.js
+++ Homework005.js
@@ -28,6 +28,7 @@ function getStringInfo(str) {
     console.log(`Количество цифр: ${quantityNumbers}`);
     console.log(`Количество других символов: ${quantitySymbols}`);
 }
+
 getStringInfo(testStr1);
 
 /*
@@ -35,8 +36,8 @@ getStringInfo(testStr1);
     Написать функцию, которая принимает двузначное число и возвращает его в текстовом виде.
     Например: 35 – тридцать пять, 89 – восемьдесят девять, 12 – двенадцать.
  */
-let num=prompt('Введите двухзначное число');
-let collectionNumber={
+let num = prompt('Введите двухзначное число');
+let collectionNumber = {
     10: 'десять',
     11: 'одинадцать',
     12: 'двенадцать',
@@ -73,22 +74,22 @@ let collectionNumber={
  * @returns {string} Строковый результат числа
  */
 function convertNumberToString(collection, number) {
-    if (+number in collection){
+    if (+number in collection) {
         return `${+number} - ${collection[number]}`;
     }
-    let firstNumber=Number(number.split('')[0]+'0');
-    let secondNumber=+number.split('')[1];
+    let firstNumber = Number(number.split('')[0] + '0');
+    let secondNumber = +number.split('')[1];
     return `${number} - ${collection[firstNumber]} ${collection[secondNumber]}`;
 }
 
-console.log(convertNumberToString(collectionNumber,num));
+console.log(convertNumberToString(collectionNumber, num));
 
 /*
     Задание 3.
     Написать функцию, которая заменяет в полученной строке большие буквы на маленькие, маленькие – на большие, а
 цифры – на знак нижнего подчеркивания.
  */
-let testStr2='jsdhfJKJH bvb985KJnkkNKN hjhHBhbJH9o$%& jjhfk4k0K7J5';
+let testStr2 = 'jsdhfJKJH bvb985KJnkkNKN hjhHBhbJH9o$%& jjhfk4k0K7J5';
 
 /**
  * Преобразует строку, в зависимости от условия, верхний/нижний регистр или знак подчеркивания
@@ -96,16 +97,14 @@ let testStr2='jsdhfJKJH bvb985KJnkkNKN hjhHBhbJH9o$%& jjhfk4k0K7J5';
  * @returns {string} Результат преобразования
  */
 function changeSymbol(str) {
-    let smallSymbol='absdefghijklmnopqrstuvwxyz';
-    let number='0123456789';
-    return str.split('').map(item=>{
-        if (smallSymbol.includes(item)){
+    let smallSymbol = 'absdefghijklmnopqrstuvwxyz';
+    let number = '0123456789';
+    return str.split('').map(item => {
+        if (smallSymbol.includes(item)) {
             return item.toUpperCase();
-        }
-        else if(smallSymbol.toUpperCase().includes(item)){
+        } else if (smallSymbol.toUpperCase().includes(item)) {
             return item.toLowerCase();
-        }
-        else if (number.includes(item)){
+        } else if (number.includes(item)) {
             return '_';
         }
         return item;
@@ -119,3 +118,125 @@ console.log(changeSymbol(testStr2));
     Написать функцию, которая преобразует названия css-стилей с дефисом в название в СamelСase стиле: font-size
 в fontSize, background-color в backgroundColor, text-align в textAlign.
  */
+let cssStyle = 'background-color';
+
+/**
+ * Преобразует имена стилей css в CamelCase
+ * @param styleName Название css стиля
+ * @returns {string} Результат преобразования в CamelCase
+ */
+function convertStyleName(styleName) {
+    return styleName.split('-').map((item, index) => {
+        if (index > 0) {
+            return item[0].toUpperCase() + item.slice(1);
+        }
+        return item;
+    }).join('');
+}
+
+console.log(convertStyleName(cssStyle));
+
+/*
+    Задание 5.
+    Написать функцию, которая принимает словосочетание и превращает его в аббревиатуру.
+    Например: cascading style sheets в CSS, объектно-ориентированное программирование в ООП.
+ */
+let text = 'cascading style sheets';
+
+function createAbbreviation(str) {
+    str = str.replace('-', ' ');
+    return str.split(' ').map(item => item[0].toUpperCase()).join('');
+}
+
+console.log(createAbbreviation(text));
+
+/*
+    Задание 6.
+    Написать функцию, которая принимает любое количество строк, объединяет их в одну длинную строку и
+возвращает ее.
+ */
+let str1 = 'My first string.';
+let str2 = 'My second string';
+let str3 = 'Hello world';
+
+/**
+ * Объединяет несколько строк в одну
+ * @param other Набор передаваемых строк
+ * @returns {string} Общая строка
+ */
+function concatString(...other) {
+    return other.join('');
+}
+
+console.log(concatString(str1, str2, str3));
+
+/*
+    Задание 7.
+    Написать функцию – калькулятор. Функция принимает строку с примером, определяет, какое действие необходимо
+выполнить (+ - * /), переводит операнды в числа, решает пример и возвращает результат.
+ */
+let strAnswer=prompt('Введите выражение вида a+b');
+
+/**
+ * Ищет результат операции
+ * @param str Строковое выражение
+ * @returns {string} Результат операции
+ */
+function findResult(str) {
+    return str+'='+eval(str);
+}
+
+console.log(findResult(strAnswer));
+
+/*
+    Задание 8.
+    Написать функцию, которая получает url и выводит подробную информацию о нем.
+Например: url “https://itstep.org/ua/about”, информация “протокол: https, домен: itstep.org, путь: /ua/about”.
+ */
+let strURL='https://itstep.org/ua/about';
+
+/**
+ * Выводит подробную информацию об адресе
+ * @param str URL-адрес
+ */
+function getInfoByUrl(str) {
+    let prt=str.split('://')[0];
+    let url=str.split('://')[1];
+    let domen=url.split('/')[0];
+    let ind=url.indexOf('/');
+    let way=url.slice(ind);
+
+    console.log(`Протокол: ${prt}; Домен: ${domen}; Путь: ${way}`);
+}
+getInfoByUrl(strURL);
+
+/*
+    Задание 9.
+    Написать функцию, которая принимает строку и разделитель и возвращает массив подстрок, разбитых с помощью
+указанного разделителя.
+Например: строка “10/08/2020”, разделитель “/”, результат: “10”, “08”, “2020”.
+Выполняя задание, не используйте функцию split().
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
