4603cc65c260953092d3cc8ebf70017706dd37b5
shinoby85 <ponk@tut.by>
2020-07-12 20:15:18 +0300

    Выполнены задачи 1 и 2. Подготовлена 3.

--- Homework006.1.js
+++ Homework006.1.js
@@ -10,3 +10,33 @@
         ■ метод, вычисляющий длину окружности.
     Продемонстрировать работу свойств и методов...
  */
+class Circle{
+    constructor() {
+        this.radius=0;
+        this.pi=3.14;
+    }
+    get fgRadius(){
+        return  this.radius;
+    }
+    set fgRadius(value){
+        this.radius=value;
+    }
+    get fgDiametr(){
+        return this.radius*2;
+    }
+    getAria(){
+        return this.pi*Math.pow(this.radius,2);
+    }
+    getLengthOfCircle(){
+        let result=2*this.pi*this.radius;
+        return Math.floor(result*100)/100;
+    }
+}
+const circle=new Circle();
+console.log(circle.fgRadius);
+circle.fgRadius=5;
+console.log(circle.fgRadius);
+console.log(circle.fgDiametr);
+console.log(circle.getAria());
+console.log(circle.getLengthOfCircle());
+
