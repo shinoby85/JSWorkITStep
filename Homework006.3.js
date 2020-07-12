72e1a90e54da6afa8f8d254b9c937071c5880001
shinoby85 <ponk@tut.by>
2020-07-12 20:53:49 +0300

    Выполнена задача 3. Подготовлена 4.

--- Homework006.3.js
+++ Homework006.3.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*Задание 3
 Реализовать класс, который описывает css класс. Класс CssClass должен содержать внутри себя:
     ■ название css класса;
@@ -7,6 +8,38 @@
     ■ метод для удаления стиля;
     ■ метод getCss(), который возвращает css код в виде строки.
 */
- class CssClass{
+class CssClass {
+    constructor(options) {
+        this.name = options.name;
+        this.style = [];
+    }
 
- }
+    setStyle(newStyle) {
+        this.style.push(newStyle);
+    }
+
+    delStyle(removeStyle) {
+        let index = this.style.findIndex(item => item[removeStyle]!==undefined);
+        this.style.splice(index, 1);
+    }
+
+    getCss() {
+        let str = `.${this.name}{`;
+        this.style.forEach(item=>{
+            for (let elem in item){
+                str+=`${elem}:${item[elem]};`;
+            }
+        });
+        str+='}';
+        return str;
+    }
+}
+let cssCode=new CssClass({
+    name:'header'
+});
+cssCode.setStyle({'color': 'purple'});
+cssCode.setStyle({'font-size': '13px'});
+cssCode.setStyle({'margin': '20px'});
+console.log(cssCode.getCss());
+cssCode.delStyle('font-size');
+console.log(cssCode.getCss());
