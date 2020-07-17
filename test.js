c35e6ce21897d5f3a480b40a7604804b83a60a80
shinoby85 <ponk@tut.by>
2020-07-17 20:03:59 +0300

    Обработка классной работы

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
 'use strict';
+let btn=document.querySelectorAll('.btn');
+let result=document.getElementById('calc-result');
+let flag=false;
+btn.forEach(item=>{
+    item.addEventListener('click',function (e) {
+        if (!flag){
+            result.value='';
+            flag=true;
+        }
+        let ch=e.target.innerText;
+        if(ch!=='='){
+            result.value+=ch;
+        }
+        else{
+            if('disabled' in result.attributes) {
+                let res = eval(result.value);
+                result.value += '=' + res;
+                flag = false;
+            }
+        }
+    });
+});
